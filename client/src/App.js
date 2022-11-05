8169159015ff65bff141a8d9dd5f213217848293
Diego <diego007.castillo@gmail.com>
2022-11-05 18:16:58 -0400

    CreatePost form and Modal first implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,  { useState } from 'react'
+import FormModal from './components/PostForm/FormModal'
+import { Button } from 'react-bootstrap'
 
 function App() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <div className="App">   
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+      <FormModal show = {show} onHide = {handleClose} />
     </div>
   );
 }
