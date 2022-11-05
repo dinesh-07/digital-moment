282a28dbfb49bff93b541fba14af744cc62124c6
muhammadsohail <mhsohail56@gmail.com>
2022-11-05 19:20:29 -0400

    style issues

--- client/src/components/Authorization/Login/index.js
+++ client/src/components/Authorization/Login/index.js
@@ -5,40 +5,27 @@ import { useTranslation } from 'react-i18next';
 
 
 const Login = ({ setModal }) => {
-
   const { t } = useTranslation()
 
   return (
-    <Form>
-      <Form.Group className="mb-3"
-      controlId="userEmail">
-        <Form.Label>{t("nav.auth.email")}</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-
-      <Form.Group className="mb-3"
-      controlId="userPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-
-      <div className='w.100 justify-content-center'>
-        <Button
-              variant="primary"
-              onClick={() => setModal(false)}
-              className='w-50 m-auto rounded-0 auth-submit'
-            >
-              Submit
-        </Button>
-
+    <Form className='d-flex flex-column align-items-center auth'>
+      <div>
+        <Form.Group className="mb-3" controlId="userEmail">
+          <Form.Label>{t("nav.auth.email")}</Form.Label>
+          <Form.Control type="email" placeholder="john@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="userPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
       </div>
-        <Button
-          variant="primary"
-          onClick={() => setModal(false)}
-          className='w-50 rounded-0 auth-submit mt-3'
-        >
-          Submit
-        </Button>
+      <Button
+        variant="primary"
+        onClick={() => setModal(false)}
+        className='w-50 rounded-0 auth-submit mt-3'
+      >
+        Submit
+      </Button>
     </Form>
   )
 }
