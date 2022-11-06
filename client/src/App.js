81db91c97a739b71b5c8caa6c0150a0f242d2046
Dinesh <dinesh.skini@gmail.com>
2022-11-06 14:34:43 -0500

    merge conflict fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import Preview from './components/Preview';
-
-function App() {
-  return (
-    <div className="App">
-      <Preview />
-=======
 import Navigation from './components/Layout/Navigation';
 import Preview from './components/Preview';
 import Footer from './components/Layout/Footer';
@@ -17,7 +9,6 @@ function App() {
       <Navigation />
       <Preview />
       <Footer />
->>>>>>> 0bf65143bd26f20fcc9f23aa8f4150b9e0ef88eb
     </div>
   );
 }
