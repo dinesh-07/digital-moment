ad4787ed86bda687f81e36e0b4620ec8983869b9
Diego <diego007.castillo@gmail.com>
2022-11-06 23:22:16 -0500

    Added Detailed Post first implementation layout

---
+++ client/src/components/DetailedPost/Post.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PostContent from './PostContent';
+import Sidebar from './Sidebar';
+import Nav from 'react-bootstrap/Nav'
+import { useTranslation } from 'react-i18next';
+const theme = createTheme();
+
+const Post = () => {
+  const { t } = useTranslation();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Nav style={{marginTop: 130, justifyContent: "center", marginBottom: 5}}>
+            <Nav.Item>
+                <Nav.Link style={ {fontSize: "40px"} }eventKey='challenge'>{t("challenge")}</Nav.Link>
+            </Nav.Item>
+            
+    </Nav>
+      <Container maxWidth="lg">
+        <main>
+          
+          <Grid container spacing={6} sx={{ mt: 10 }}>
+            <PostContent title="From the firehose"/>
+            <Sidebar/>
+          </Grid>
+        </main>
+
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default Post;
