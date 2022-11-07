de49e893584de124cdd7a14380cb6e77790ed03c
Dinesh <dinesh.skini@gmail.com>
2022-11-06 23:51:59 -0500

    deployment changes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const { connect, disconnect } = require('./helper/connection');
 
+const app = express();
+
+// server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Use Routes
 const users = require('./routes/users');
 const ideas = require('./routes/ideas');
 const challenges = require('./routes/challenges');
@@ -12,8 +24,6 @@ const auth = require('./routes/auth');
 
 const errorHandlerMiddleware = require('./middleware/error-handler.middleware');
 const loggerMiddleware = require('./middleware/logger.middleware');
-
-const app = express();
 
 try {
   connect();
