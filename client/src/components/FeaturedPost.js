92c685b6d78736b5f42f760949e417da7e469ec3
Dinesh <dinesh.skini@gmail.com>
2022-11-07 01:20:28 -0500

    format date to DD-MM-YYYY

--- client/src/components/FeaturedPost.js
+++ client/src/components/FeaturedPost.js
@@ -1,5 +1,4 @@
-import { useState} from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
@@ -7,71 +6,73 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { useTranslation } from 'react-i18next';
-import { tags } from "./Dashboard/tags"
+import { tags } from './Dashboard/tags';
+import EmojiFlagsOutlinedIcon from '@mui/icons-material/EmojiFlagsOutlined';
+import FlagIcon from '@mui/icons-material/Flag';
+import Moment from 'moment';
 
-
-
-function getImage(tag){
-  for (let i = 0; i < tags.length; i++){
-    if (tags[i].value === tag){
+function getImage(tag) {
+  for (let i = 0; i < tags.length; i++) {
+    if (tags[i].value === tag) {
       return tags[i].image;
     }
   }
-  return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOpRO11pfNTX5nuwEvx7Zd_Y0_kfmo8-ioXSFL2RI3&s"
+  return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOpRO11pfNTX5nuwEvx7Zd_Y0_kfmo8-ioXSFL2RI3&s';
 }
-import { Button } from '@mui/material';
-import EmojiFlagsOutlinedIcon from '@mui/icons-material/EmojiFlagsOutlined';
-import FlagIcon from '@mui/icons-material/Flag';
 
 function FeaturedPost(props) {
   const { post } = props;
   const { t } = useTranslation();
   const [upvotes, setUpvotes] = useState(Math.floor(Math.random() * 1000));
   const [upvoted, setUpvoted] = useState(false);
-  const handleUpvote = e => {
+  const handleUpvote = (e) => {
     e.stopPropagation();
     e.preventDefault();
-    
+
     if (upvoted) {
-      setUpvoted(false); 
-      setUpvotes(upvotes - 1)
+      setUpvoted(false);
+      setUpvotes(upvotes - 1);
+    } else {
+      setUpvoted(true);
+      setUpvotes(upvotes + 1);
     }
-    else {
-      setUpvoted(true) 
-      setUpvotes(upvotes + 1)
-    }
-    
-  }; 
-
-  
-
+  };
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href='#'>
+      <CardActionArea component="a" href="#">
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
-            <div className='d-flex gap-2 upvoter' onMouseDown={(e) => {e.stopPropagation()}}>
-              {upvoted ? 
-                <FlagIcon style={{ color: "rgb(238, 181, 80)" }} onClick = { handleUpvote } />
-              :
-                <EmojiFlagsOutlinedIcon style={{color: "rgb(79, 126, 225)"}} onClick = { handleUpvote } />
-              }
-              <Typography>
-                {upvotes}
-              </Typography>
+            <div
+              className="d-flex gap-2 upvoter"
+              onMouseDown={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              {upvoted ? (
+                <FlagIcon
+                  style={{ color: 'rgb(238, 181, 80)' }}
+                  onClick={handleUpvote}
+                />
+              ) : (
+                <EmojiFlagsOutlinedIcon
+                  style={{ color: 'rgb(79, 126, 225)' }}
+                  onClick={handleUpvote}
+                />
+              )}
+              <Typography>{upvotes}</Typography>
             </div>
             <Typography component="h2" variant="h5">
               {post.name}
             </Typography>
             <Typography variant="subtitle1" color="text.secondary">
-              {post.createdAt}
+              {Moment(post.createdAt).format('DD-MM-YYYY')}
             </Typography>
             <Typography variant="subtitle1" paragraph>
               {post.description}
             </Typography>
             <Typography variant="subtitle1" color="primary">
-              {t("post")}
+              {t('post')}
             </Typography>
           </CardContent>
           <CardMedia
@@ -85,7 +86,5 @@ function FeaturedPost(props) {
     </Grid>
   );
 }
-
-
 
 export default FeaturedPost;
--- client/src/components/Posts/postPreviewList.js
+++ client/src/components/Posts/postPreviewList.js
@@ -1,29 +1,27 @@
-import PostPreviewCard from "./postPreviewCard";
-import CardGroup from "react-bootstrap/CardGroup"
-import React from 'react'
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import Container from "react-bootstrap/Container";
+import PostPreviewCard from './postPreviewCard';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
-function postPreviewList({cardInfo}) {
-    console.log(cardInfo)
+function postPreviewList({ cardInfo }) {
   return (
     <div>
-        <Container>
-            <Col>
-                <Row>
-                {cardInfo.map((post, i) => (
-                    <PostPreviewCard Title={post.Title} User={post.User} Content={post.Content} Thumbnail={post.Thumbnail} />
-                ))}
-                </Row>
-        
-            </Col>
-        </Container>
-        
-            
-        
+      <Container>
+        <Col>
+          <Row>
+            {cardInfo.map((post, i) => (
+              <PostPreviewCard
+                Title={post.Title}
+                User={post.User}
+                Content={post.Content}
+                Thumbnail={post.Thumbnail}
+              />
+            ))}
+          </Row>
+        </Col>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default postPreviewList
+export default postPreviewList;
